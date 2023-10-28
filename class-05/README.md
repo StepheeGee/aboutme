0d0e95d30b38cfc477855d6088a2d3a0c57983c4
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-10-28 00:01:51 -0400

    final version of aboutme

--- class-05/README.md
+++ class-05/README.md
@@ -15,10 +15,24 @@ The final improvement on the About Me webpage.
 
 
 ### Lighthouse Accessibility Report Score
-![LighthouseReport](Not Done)
+
+![LighthouseReport](photos/LighthouseScore.png)
 
 
 ### Reflections and Comments
+
+I think I did a pretty good job turning the questions into functions - especially using a function for the first five questions. I also implemented some of the feedback that I received on my prior submissions, including having an algorithm to show all the correct answers in the array. 
+
+Learning Journal (Class 05)
+
+*"Adults need to know why to learn something; as soon as they answer this question, they are ready to start."*
+
+This definitely rings true for me. Understanding the 'why' behind what I'm learning provides context, and with context comes greater comprehension. Otherwise, the dots won't connect and I'll find myself stuck in a loop trying to solve for 'why'. (Pun intended)
+
+During today's class demo, where we revisited the topic of JS functions, I experienced a small but significant "aha" moment. Previously, I couldn't quite grasp the utility of functions in our lab exercises. However, today's demonstration shed light on how functions can be used to consolidate our quiz questions, allowing for the efficient execution of multiple questions. That made so much sense to me.
+
+I now understand the purpose of functions in a real-world scenario. They serve as a way to encapsulate and streamline code, making it more organized and easier to manage. I might not know exactly how to create such an overarching function yet, but the understanding that I gained from the demo has made me kind of determined to figure out a similar solution for my AboutMe project.
+I'm glad I actually feel inclined to explore the material further, experiment with it, and eventually incorporate it into my projects. It serves as a reminder that comprehending the 'why' is the catalyst for the learning process, moving me forward with a sense of understanding.
 
 
 
--- class-05/script.js
+++ class-05/script.js
@@ -1,142 +1,111 @@
-
 'use strict';
-
 
 let score = 0;
 
-function newQuestion(question, rightAnswer, goodJobMessage, wrongMessage) {
-  let userAnswer = prompt(question).toLowerCase();
-  console.log(`${question} User's answer: ${userAnswer}`);
-  
-  if (userAnswer === rightAnswer) {
+function askQuestion(question, correctAnswer, goodJobMessage, wrongMessage) {
+  let userAnswer = prompt(question);
+
+  if (userAnswer && userAnswer.toLowerCase() === correctAnswer) {
     score++;
-    console.log(goodJobMessage);
     alert(goodJobMessage);
   } else {
-    console.log(wrongMessage);
     alert(wrongMessage);
   }
 }
 
-function newQuiz() {
+function quizQuestions() {
   let userName = prompt("What is your name?");
-  console.log(`Name entered: ${userName}`);
-
 
   if (userName) {
     alert(`Hi ${userName}! We're going to play a game where I ask you seven questions. Press OK to begin`);
-    
 
-    newQuestion(
+    askQuestion(
       `${userName}, does Steph live in Atlanta? (Type 'yes' or 'no')`,
       "yes",
       "Correct! Steph lives in Atlanta.",
-      "Incorrect. Steph does live in Atlanta."
+      "Incorrect. Steph does not live in Atlanta."
     );
 
-
-    newQuestion(
+    askQuestion(
       "Does Steph have children? (Type 'yes' or 'no')",
       "no",
       "Great job! Steph doesn't have children.",
       "Incorrect. Steph doesn't have children."
     );
 
-
-    newQuestion(
+    askQuestion(
       "Does Steph have a pet? (Type 'yes' or 'no')",
       "yes",
       "Correct! Steph has a dog named Tot.",
       "Incorrect. Steph has a dog named Tot."
     );
 
-
-    newQuestion(
+    askQuestion(
       "Is Steph an attorney? (Type 'yes' or 'no')",
       "yes",
       "Congratulations! Steph is an attorney.",
       "Incorrect. Steph is an attorney."
     );
 
-
-    newQuestion(
+    askQuestion(
       "Does Steph like pizza? (Type 'yes' or 'no')",
       "yes",
       "Correct! Steph likes pizza.",
       "Incorrect. Steph likes pizza."
     );
 
-
-    alert(`Your final score is: ${score} out of 5. Well done!`);
+    alert(`Your current score is: ${score} out of 5. Only two more questions to go!`);
   } else {
     alert("Please enter your name to continue.");
   }
 }
 
+function guessYearsAsAttorney() {
+  for (let i = 1; i <= 4; i++) {
+    let guess = parseInt(prompt('How many years has Steph been an attorney? Guess a number 0 - 20.'));
+    const correctAnswer = 10;
 
-newQuiz();
-
-
-for (let i = 1; i <= 4; i++) {
-  let guess = prompt('How many years has Steph been an attorney? Guess a number 0 - 20.');
-  let correctAnswer = 10;
-  guess = parseInt(guess);
-
-  if (guess === correctAnswer) {
-    score++;
-    console.log('You are correct!');
-    alert(`Correct! Your current score is: ${score}`);
-    break;
-  } else if (guess < correctAnswer) {
-    console.log('Too low');
-    alert(`Too low. You have ${4-i} tries left. Try again.`);
-  } else {
-    console.log('Too high');
-    alert(`Too high. You have ${4-i} tries left. Try again.`);
-    
-    if (i === 4) {
-    console.log(`You have used all your attempts. The correct answer is ${correctAnswer}.
-    Your current score is: ${score}.`);
-    alert(`You have used all your attempts. The correct answer is ${correctAnswer}
-    Your current score is: ${score}.`);
-    }
-  }
-}
-
-
-
-let correctAnswer = ["cheese", "pepperoni", "mushrooms", "olives", "italian sausage", "pineapples"];
-let attempts = 6;
-
-while (attempts > 0) {
-  let pizza = prompt(`What pizza topping does Steph like? You have ${attempts} attempts remaining.`).toLowerCase();
-
-  if (correctAnswer.includes(pizza)) {
-    score++;
-    console.log('You are correct!');
-    alert(`You are correct! Your final score is: ${score} out of 7. Well done!`);
-    break;
-  } else {
-    attempts--;
-    if (attempts > 0) {
-      console.log('Nope.');
-      alert(`Nope. You have ${attempts} tries left. Try again.`);
+    if (guess === correctAnswer) {
+      score++;
+      alert(`Correct! Your current score is: ${score}`);
+      break;
+    } else if (guess < correctAnswer) {
+      alert(`Too low. You have ${4 - i} tries left. Try again.`);
     } else {
-      console.log('You have used all your attempts.');
-      alert(`You have used all your attempts. Your final score is: ${score} out of 7. Well done!`);
+      alert(`Too high. You have ${4 - i} tries left. Try again.`);
+    }
+
+    if (i === 4) {
+      alert(`You have used all your attempts. The correct answer is ${correctAnswer}. Your current score is: ${score}.`);
+    }
+  }
+}
+
+function guessPizzaTopping() {
+  const correctAnswer = ["cheese", "pepperoni", "mushrooms", "olives", "italian sausage", "pineapples"];
+  let attempts = 6;
+  let correctToppings = [];
+
+  while (attempts > 0) {
+    let pizza = prompt(`What pizza topping does Steph like? You have ${attempts} attempts remaining.`).toLowerCase();
+
+    if (correctAnswer.includes(pizza)) {
+      score++;
+      correctToppings.push(pizza);
+      alert(`You are correct! Your final score is: ${score} out of 7. Well done!`);
+      break;
+    } else {
+      attempts--;
+      if (attempts > 0) {
+        alert(`Nope. You have ${attempts} tries left. Try again.`);
+      } else {
+        alert(`You have used all your attempts. The correct toppings are: ${correctAnswer.join(", ")}. Your final score is: ${score} out of 7. Well done!`);
+      }
     }
   }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
+quizQuestions();
+guessYearsAsAttorney();
+guessPizzaTopping();
